ce9cdde38ae0e584dc794dc383efda7339d8fa77
Mike_col <mariasf@correo.udistrital.edu.co>
2020-08-04 17:06:23 -0500

    solve

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -1,20 +1,20 @@
-import path from 'path'
-import express from 'express'
-import webpack from 'webpack'
-import webpackDevMiddleware from 'webpack-dev-middleware'
-import webpackHotMiddleware from 'webpack-hot-middleware'
-import config from '../../webpack.dev.config.js'
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from '../../webpack.dev.config.js';
 
-const app = express(),
-            DIST_DIR = __dirname,
-            HTML_FILE = path.join(DIST_DIR, 'index.html'),
-            compiler = webpack(config)
+const app = express();
+DIST_DIR = __dirname;
+HTML_FILE = path.join(DIST_DIR, 'index.html');
+compiler = webpack(config);
 
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath
-}))
+}));
 
-app.use(webpackHotMiddleware(compiler))
+app.use(webpackHotMiddleware(compiler));
 
 app.get('*', (req, res, next) => {
   compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
@@ -25,11 +25,11 @@ app.get('*', (req, res, next) => {
   res.send(result)
   res.end()
   })
-})
+});
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`App listening to ${PORT}....`)
     console.log('Press Ctrl+C to quit.')
-})
+});
