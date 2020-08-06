884d25f8696707f1eae2dd0eeadf1d603d4464cb
Mike_col <mariasf@correo.udistrital.edu.co>
2020-08-05 21:23:00 -0500

    CSS

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -88,6 +88,16 @@ module.exports = {
           'css-loader',
         ],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            } 
+        },
+      }
     ]
   },
   plugins: [
