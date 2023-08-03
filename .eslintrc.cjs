4c56dc86f1c9ecbc00367b23d6d273e716765723
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-08-03 00:16:31 -0500

    feat:

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,5 +13,7 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended'
     ],
+    parser: '@typescript-eslint/parser'
 }
