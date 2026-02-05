06ac7afd8c0c0b376fe8dfa9cb238273a183f9f6
Maikpwwq <mariasf@udistrital.edu.co>
2026-02-04 22:36:56 -0500

    fix: disable SSR for failing pages, restore content

--- pages/(app)/directorio-requerimientos/+config.ts
+++ pages/(app)/directorio-requerimientos/+config.ts
@@ -1,4 +1,5 @@
 export default {
   title: 'Directorio de Requerimientos | Comunidad Dezzpo',
-  description: 'Explora requerimientos de proyectos activos y aplica con cotizaciones.'
+  description: 'Explora requerimientos de proyectos activos y aplica con cotizaciones.',
+  ssr: false,
 }
